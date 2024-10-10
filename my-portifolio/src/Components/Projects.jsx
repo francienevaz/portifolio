64fa2b7592cf63843f2e3c4138486c73b5bbee92
francienevaz <fran.vfraga@gmail.com>
2024-10-10 20:06:13 -0300

    updated layout

--- my-portifolio/src/Components/Projects.jsx
+++ my-portifolio/src/Components/Projects.jsx
@@ -1,13 +1,48 @@
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css'
 import '../Styles/Projects.css'
 
 const Projects = () => {
     return(
-        <div className="projects">
-            <div className='item'>Project I</div>
-            <div className='item'>Project II</div>
-            <div className='item'>Project III</div>
-            <div className='item'>Project IV</div>
-        </div>
+        <>
+            <div className="container">
+                <Carousel>
+                    <Carousel.Item>
+                    <div className="card">
+                        <div className='card-wrapper'>
+                            <h2>Projeto I</h2>
+                            <p>Site</p>
+                        </div>
+                    </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <div className="card">
+                        <div className='card-wrapper'>
+                            <h2>Projeto II</h2>
+                            <p>CRUD</p>
+                        </div>
+                    </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <div className="card">
+                        <div className='card-wrapper'>
+                            <h2>Projeto III</h2>
+                            <p>API</p>
+                        </div>
+                    </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <div className="card">
+                        <div className='card-wrapper'>
+                            <h2>Projeto IV</h2>
+                            <p>E-commerce</p>
+                        </div>
+                    </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </>
+        
     )
 }
 
